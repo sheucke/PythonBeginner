e9fa3d491fac204963f95e05185679261aae7909
Sebastian Heucke <sebastian.heucke@med.uni-muenchen.de>
2020-09-29 12:36:26 +0200

    input functions

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ price = 19.95  # float
 first_name = "Sebastian"  # string
 is_online = False  # bool
 print(age)
+
+name = input("What is your name?:  ")
+print(f"Hello {name}.")
+
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year)
+print(f"Your age is: {age}")
