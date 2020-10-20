da29e900de76d372be603afe8512428007757863
sebastian <sebastian.heucke@med.uni-muenchen.de>
2020-10-20 09:26:48 +0200

    new changes

--- app.py
+++ app.py
@@ -4,9 +4,24 @@ first_name = "Sebastian"  # string
 is_online = False  # bool
 print(age)
 
-name = input("What is your name?:  ")
-print(f"Hello {name}.")
+# name = input("What is your name?:  ")
+# print(f"Hello {name}.")
 
-birth_year = input("Enter your birth year: ")
-age = 2020 - int(birth_year)
-print(f"Your age is: {age}")
+# birth_year = input("Enter your birth year: ")
+# age = 2020 - int(birth_year)
+# print(f"Your age is: {age}")
+
+course = "Python for Beginners"
+print(course.find('y'))
+print(course.replace('for', '4'))
+print('Python' in course)
+
+
+weight = int(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print(f"Weight in Lbs: {converted:.2f}")
+else:
+    converted = weight * 0.45
+    print(f"Weight in Kgs: {converted:.2f}")
